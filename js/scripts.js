ce9b83d76c5550417d3555688d5069c3f24bae57
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-03-20 03:01:09 -0700

    add some user interface to index.html, display the name of the winner once it is determined and add a secondary condition to the if statement on line 83 that will 'lock' the board after a winner is determined

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ Board.prototype.findSpot = function(x, y) {
 }
 
 Board.prototype.checkForWin = function(board) {
+	var win = false;
 	var winningCombos = [[[0,0],[1,0],[2,0]],
 											 [[0,1],[1,1],[2,1]],
 											 [[0,2],[1,2],[2,2]],
@@ -30,16 +31,23 @@ Board.prototype.checkForWin = function(board) {
 		marks.forEach(function(mark) {
 			if ((board.findSpot((winningCombo[0][0]), (winningCombo[0][1])) == mark) && (board.findSpot((winningCombo[1][0]), (winningCombo[1][1])) == mark) &&
 			(board.findSpot((winningCombo[2][0]), (winningCombo[2][1])) == mark)) {
-				alert('winner');
+				win = true;
 			}
 		});
 	});
+	return win;
 }
 
 Board.prototype.checkForTie = function(turnCount) {
 	if (turnCount === 9) {
-		alert('tie');
+		return true;
 	}
+}
+
+Board.prototype.reset = function() {
+	this[0] = [[],[],[]];
+	this[1] = [[],[],[]];
+	this[2] = [[],[],[]];
 }
 
 var turnPlayer = function(turnCount, player1, player2) {
@@ -53,25 +61,42 @@ var turnPlayer = function(turnCount, player1, player2) {
 
 //ui logic
 $(function() {
-	var player1Name = prompt('Player 1 enter your name:')
-	var player2Name = prompt('Player 2 enter your name:')
-	var gameBoard = new Board();
-	var player1 = new Player(player1Name, 'X');
-	var player2 = new Player(player2Name, 'O')
-	var turnCount = 0;
+	$('form#players').submit(function(event) {
+		var player1Name = $('input#player1name').val();
+		var player2Name = $('input#player2name').val();
+		$(this).hide()
+		$('#board').show();
+
+		gameBoard = new Board();
+	 	player1 = new Player(player1Name, 'X');
+	 	player2 = new Player(player2Name, 'O')
+		turnCount = 0;
+		event.preventDefault();
+	});
+
 
 	$('.col-xs-4').each(function() {
 		$(this).click(function() {
 			var boardCoordX = $(this).data('xcoord');
 			var boardCoordY = $(this).data('ycoord');
-			if (gameBoard.findSpot(boardCoordX, boardCoordY).length === 0) {
+
+			if ((gameBoard.findSpot(boardCoordX, boardCoordY).length === 0) && !(gameBoard.checkForWin(gameBoard))) {
 				turnCount += 1;
 				gameBoard.checkForTie(turnCount);
 				$(this).text(turnPlayer(turnCount, player1, player2).mark);
 				gameBoard.findSpot(boardCoordX, boardCoordY).push(turnPlayer(turnCount, player1, player2).mark);
-				// console.log(gameBoard.findSpot(boardCoordX, boardCoordY));
 			}
-			gameBoard.checkForWin(gameBoard);
+
+			if (gameBoard.checkForWin(gameBoard)) {
+				$('#victory').show();
+				$('span#winner-name').text(turnPlayer(turnCount, player1, player2).name)
+			}
 		});
 	});
+
+	$('button#play-again').click(function() {
+		$('.col-xs-4').empty();
+		gameBoard.reset(gameBoard);
+		$(this).parent().hide();
+	})
 });
